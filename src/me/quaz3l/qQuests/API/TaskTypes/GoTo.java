5d82f73b51ec2e53218587cb1539ab2621d13d52
Sam Schooler <schooler.sam@gmail.com>
2013-01-25 10:19:22 -0600

    Setup goto task type

--- src/me/quaz3l/qQuests/API/TaskTypes/GoTo.java
+++ src/me/quaz3l/qQuests/API/TaskTypes/GoTo.java
@@ -1,5 +1,27 @@
 package me.quaz3l.qQuests.API.TaskTypes;
 
-import org.bukkit.event.Listener;
+import org.bukkit.Location;
 
-public class GoTo implements Listener {}
+import me.quaz3l.qQuests.qQuests;
+
+public class GoTo {
+	public void launchAgent(String player, Location location1, Location location2) {
+		Location loc1 = location1.getBlock().getLocation();
+		Location loc2 = location2.getBlock().getLocation();
+		qQuests.plugin.getServer().getScheduler().runTaskTimer(qQuests.plugin, new Runnable() {
+			@Override
+			public void run() {
+				// TODO Cuboid script
+			}
+		}, 5 * 20, 5 * 20); // Update every 5 seconds
+	}
+	public void launchAgent(String player, Location location, double radius) {
+		Location loc = location.getBlock().getLocation();
+		qQuests.plugin.getServer().getScheduler().runTaskTimer(qQuests.plugin, new Runnable() {
+			@Override
+			public void run() {
+				// TODO Radius script
+			}
+		}, 5 * 20, 5 * 20); // Update every 5 seconds
+	}
+}
